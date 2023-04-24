b1bb9a013e240ac82c647bb17850f041ff50cee2
Milad-Rufai <temilader19@gmail.com>
2023-04-24 12:05:22 +0100

    Final Fix

--- profile-app/src/App.js
+++ profile-app/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Search from './Search';
-import Profile from './Profile';
 
 function App() {
   return ([
     <div className='App-body'>
       <Search></Search>
-      <Profile></Profile>
     </div>
 
   ]);
--- profile-app/src/search.js
+++ profile-app/src/search.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './Search.css';
+import './Profile.css';
 import { useState } from 'react';
 import { Users } from './Users'
-import(Users)
 
 
 
@@ -13,17 +13,27 @@ function Search() {
     setSearchTerm(event.target.value);
   };
 
-  const filteredData = Users.filter((itemlog) =>
-    itemlog.firstName.toLowerCase().includes(searchTerm.toLowerCase()));
+  const filteredData = Users.filter((Users) =>
+    Users.firstName.toLowerCase().includes(searchTerm.toLowerCase()));
 
   return (
     <div>
       <input className='search-box' type="text" placeholder="Search" value={searchTerm} onChange={handleSearch} />
+      <div className='info'>
       <ul>
-      {filteredData.map((itemlog) => (
-          <li key={itemlog.firstName}>{itemlog.firstName}</li>
-        ))}
+      {filteredData.map((Users) => {
+        return(
+        
+          <li className='item' key={Users.firstName}>{<><div>
+            <img src={Users.picture} alt='1'></img>
+            </div>
+            <div>
+            <p>{Users.id}</p>
+            <p>{Users.title} {Users.firstName} {Users.lastName}</p>
+            </div></>}</li> )
+        })}
       </ul>
+    </div>
     </div>
   );
 }
